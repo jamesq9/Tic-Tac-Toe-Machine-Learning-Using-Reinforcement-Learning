a588d9aac0015f4212e298cfd2c879a1c6998bf3
James Ballari <james.q917@gmail.com>
2017-07-19 10:03:11 +0530

    Update deep_reinforcement_learning.py

--- deep_reinforcement_learning.py
+++ deep_reinforcement_learning.py
@@ -1,4 +1,4 @@
-# A Python Program for implementing Machine Learning for the Game Tic Tac Toe (3x3) using Reinforcement Learning, using the technique of Q learning  
+# A Python Program to implement Machine Learning for the Game Tic Tac Toe (3x3) using Reinforcement Learning (Q learning technique) and tensorflow. 
 # 
 #   Note: mistakes were made, especially spelling mistakes.
 #   
